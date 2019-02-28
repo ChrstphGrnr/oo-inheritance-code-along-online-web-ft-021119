137af9a75ecea9902b0ecd20fa885a9b4f45f962
Christoph Gruener <christophgruener@me.com>
2019-02-28 21:29:35 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,18 @@
 class Vehicle
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size 
+    @wheel_number = wheel_number
+  end
+
+  def go
+    "vrrrrrrooom!"
+  end
+
+  def filling_up_tank
+    "filling up!"
+  end
 
 end
+
